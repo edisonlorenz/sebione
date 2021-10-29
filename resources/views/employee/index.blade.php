d43e64ed9df1c32148c0e1a2dc77fe1fa99b11a5
edisonlorenz <edisonlorenz84@gmail.com>
2021-10-29 02:47:50 -0700

    Auth and UI

---
+++ resources/views/employee/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h2>employee</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\EmployeeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('login'));
 });
+Route::resource('company', CompanyController::class);
+Route::resource('employee', EmployeeController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
